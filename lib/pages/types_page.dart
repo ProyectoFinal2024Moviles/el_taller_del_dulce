f18fd8e019a389e8bac4bf814ccc7b9b013d54f0
Santiago Garcia <61425186+sachi08@users.noreply.github.com>
2024-04-01 20:01:50 -0500

    actualizacion de base

---
+++ lib/pages/types_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TypesPage extends StatefulWidget {
+  const TypesPage({super.key});
+
+  @override
+  State<TypesPage> createState() => _TypesPageState();
+}
+
+class _TypesPageState extends State<TypesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
